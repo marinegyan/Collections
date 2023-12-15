42e9b549109d14189669267775854b8bd9a08e16
marinegyan <marine.galstyan.123@gmail.com>
2023-12-15 19:54:38 +0400

    add method to HashSet was added

--- src/set/HashSet.java
+++ src/set/HashSet.java
@@ -3,25 +3,37 @@ package set;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HashSet<K> {
+public class HashSet<V> {
+    private final int arrSize = 16;
+    private final List<List<Entry<V>>> list = new ArrayList<>(arrSize);
+
+    public void add(V value) {
+        int index = value.hashCode() % arrSize;
+       List<Entry<V>> bucket  = list.get(index);
+       for(int i=0;i< bucket.size();i++){
+           Entry<V> x = bucket.get(i);
+           if(x.getValue().equals(value)){
+               return;
+           }
+           bucket.add(new Entry<>(value));
+       }
+    }
 
 
 
 
+    public static class Entry<V>{
+    private V value;
 
-
-public static class Entry<K>{
-    private K value;
-
-    public Entry(K value) {
+    public Entry(V value) {
         this.value = value;
     }
 
-    public K getValue() {
+    public V getValue() {
         return value;
     }
 
-    public void setValue(K value) {
+    public void setValue(V value) {
         this.value = value;
     }
 }
