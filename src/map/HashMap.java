d7f99a33a4b2a436f9f66068c921339077793cbc
marinegyan <marine.galstyan.123@gmail.com>
2023-12-15 20:32:35 +0400

    Entry inner class was added

--- src/map/HashMap.java
+++ src/map/HashMap.java
@@ -1,4 +1,50 @@
 package map;
 
-public class HashMap {
+import set.HashSet;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashMap<K, V> {
+
+
+
+
+    public static class Entry<K,V> {
+        private V value;
+        private K key;
+
+        public Entry(K key,V value) {
+            this.value = value;
+            this.key = key;
+
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public String toString() {
+            return
+                    "value=" + value +
+                    ", key=" + key
+                    ;
+        }
+    }
 }
+
+
