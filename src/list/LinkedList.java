a63cf4cb78c239720f2e52864de6d1100166886b
marinegyan <marine.galstyan.123@gmail.com>
2023-12-15 23:34:10 +0400

    LinkedList methods were added

--- src/list/LinkedList.java
+++ src/list/LinkedList.java
@@ -1,7 +1,59 @@
 package list;
 
 public class LinkedList {
+    private Node head;
+    private int size = 0;
 
+    public void add(Integer value) {
+        if (head == null) {
+            head = new Node();
+            head.value = value;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node();
+            temp.next.value = value;
+        }
+        size++;
+    }
 
+    public void remove(Integer value) {
+        if (head == null) {
+            return;
+        }
+        if (head.value.equals(value)) {
+            head = head.next;
+        } else {
+            Node temp = head;
+            while (temp.next != null && !temp.next.value.equals(value)) {
+                temp = temp.next;
+            }
+            if (temp.next != null) {
+                temp.next = temp.next.next;
+            }
+        }
+        size--;
+    }
+    @Override
+    public String toString() {
+        return "LinkedList{" +
+                "head=" + head +
+                ", size=" + size +
+                '}';
+    }
 
+    private static class Node {
+        private Node next;
+        Integer value;
+
+        @Override
+        public String toString() {
+            return
+                    "next=" + next +
+                            "value=" + value;
+
+        }
+    }
 }
--- src/list/Main.java
+++ src/list/Main.java
@@ -15,6 +15,16 @@ public class Main {
         System.out.println(arrayList.toString());
         System.out.println(arrayList.contains(1));
 
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(3);
+        linkedList.add(3);
+        System.out.println(linkedList.toString());
+        linkedList.remove(2);
+        System.out.println(linkedList.toString());
+
 
     }
 }
